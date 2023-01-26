26dfe3f60a4409c1fe30c9bdf28505633b0085c3
yotskovich <yatskevichaliaksei.11@gmail.com>
2023-01-26 21:25:38 +0100

    Add datetime

--- text.py
+++ text.py
@@ -1,5 +1,9 @@
+import datetime
+
 name = input("What is you name? ")
 print('Hello, ', name)
 
 print('We learn Git')
 print('We lear GitHub')
+print("Today", datetime.today().strtime('%d/%m/%Y'))
+
